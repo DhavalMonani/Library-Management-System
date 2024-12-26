bdfa720695842f5456b485c7a4429afb4d922828
DhavalMonani <dhvlmonani@gmail.com>
2024-12-26 12:40:05 +0530

    Created class based structure for the Library Management System

---
+++ main.py
@@ -0,0 +1,23 @@
+class Library:
+
+    def __init__():
+        self.books = []
+    ##
+
+    def add_books(self):
+        pass
+    ##
+
+    def borrow_books(self):
+        pass
+    ##
+
+    def return_books(self):
+        pass
+    ##
+
+    def view_available_books(self):
+        pass
+    ##
+
+##
