283f5ea22ee8d7a9771b58e623c4c8d46fbaed48
DhavalMonani <dhvlmonani@gmail.com>
2024-12-26 16:18:41 +0530

    Solved the Edge case for borrow book feature

--- test_library.py
+++ test_library.py
@@ -3,23 +3,22 @@ from main import Library
 
 class LibraryTestCases(unittest.TestCase):
     def setUp(self):
+        # Initialize a new instance of Library before each test
         self.Librarian = Library()
-    ##
+        # Add some initial books
+        self.Librarian.add_book("1", "The Alchemist", "Paulo Coelho", "1988")
 
     def test_add_book(self):
         result = self.Librarian.add_book("2", "Harry Potter", "J.K. Rowling", "1997")
-        self.assertEqual(result,"Book Added Successfully!")
-    ##
+        self.assertEqual(result, "Book Added Successfully!")
 
     def test_borrow_book(self):
+        # Borrow the book
         result = self.Librarian.borrow_book("The Alchemist")
-        self.assertEqual(result,"Book Borrowed Successfully!")
-    ##
+        self.assertEqual(result, "Book Borrowed Successfully!")
 
-    def test_borrow_book2(self):
+        # Attempt to borrow the same book again, which should raise an error
         with self.assertRaises(ValueError) as context:
             self.Librarian.borrow_book("The Alchemist")
         self.assertEqual(str(context.exception), 'Sorry, "The Alchemist" is currently borrowed.')
-    ##
-
 
