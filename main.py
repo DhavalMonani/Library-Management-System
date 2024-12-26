91739efc849088d534dca067d79332f5f7888cdd
DhavalMonani <dhvlmonani@gmail.com>
2024-12-26 13:14:47 +0530

    Implemented the add book feature

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 class Library:
 
-    def __init__():
+    def __init__(self):
         self.books = []
     ##
 
-    def add_books(self):
-        pass
+    def add_books(self, isbn, title, author, publication_year):
+        new_book = {
+            "ISBN": isbn,
+            "title": title,
+            "author": author,
+            "publication_year": publication_year,
+            "status": "available"
+        }
+        self.books.append(new_book)
+        return "Book Added Successfully!"
     ##
 
     def borrow_books(self):
--- test_library.py
+++ test_library.py
@@ -1,9 +1,10 @@
 import unittest
-import add_books from main
+from main import Library
 
 class LibraryTestCases(unittest.TestCase):
     
     def test_add_books(self):
-        result = add_books("1", "The Alchemist", "Paulo Coelho", "1988")
+        Librarian = Library()
+        result = Librarian.add_books("1", "The Alchemist", "Paulo Coelho", "1988")
         self.assertEqual(result,"Book Added Successfully!")
     ##
